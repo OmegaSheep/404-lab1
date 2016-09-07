2b473cd3984f92bceb90196374d56c2a817eefab
SuperSheep18 <nothingwillgowrong@gmail.com>
2016-09-07 15:13:00 -0600

    yay

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,4 +1,4 @@
 import requests
 
-response = requests.post('http://ccid-eddieantonio.rhcloud.com/aedan')
-print response.status_code
+response = requests.get('https://raw.githubusercontent.com/SuperSheep18/404-lab1/master/lab1/lab1.py')
+print response.text
